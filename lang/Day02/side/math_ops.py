0d8245af01221f489521a497dd4dcd4d560d5f6a
Paul McQuade <paulmcquad@gmail.com>
2025-08-27 21:01:29 +0100

    BMI Calculator
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day02/side/math_ops.py
@@ -0,0 +1,13 @@
+print("My age: " + str(12))
+print(123 + 456)
+print(7 - 3)
+print(3 * 2)
+print(6 / 3)
+#print(type(6 / 3))
+print(6 // 3)
+print(2 ** 2)
+
+# PEMDAS Operations
+
+print(3 * 3 + 3 / 3 - 3)
+print(3 * (3 + 3) / 3 - 3)

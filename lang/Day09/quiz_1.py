cf42ad4aeb57e12cd2fc7e8526d778f4e5dc6939
Paul McQuade <paulmcquad@gmail.com>
2025-09-15 23:27:26 +0100

    Day 09
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day09/quiz_1.py
@@ -0,0 +1,26 @@
+student_scores = {
+    'Harry': 88,
+    'Ron': 78,
+    'Hermione': 95,
+    'Draco': 75,
+    'Neville': 60
+}
+
+# Create an empty dictionary to collect the new values.
+student_grades = {}
+
+# Loop through each key in the student_scores dictionary
+for student in student_scores:
+
+    # Get the value (student score) by using the key each time.
+    score = student_scores[student]
+
+    # Check what grade the score would get, then add it to student_grades
+    if score >= 91:
+        student_grades[student] = 'Outstanding'
+    elif score >= 81:
+        student_grades[student] = 'Exceeds Expectations'
+    elif score >= 71:
+        student_grades[student] = 'Acceptable'
+    else:
+        student_grades[student] = 'Fail'
---
+++ lang/Day09/quiz_2.py
@@ -0,0 +1,36 @@
+# starting_dictionary = {
+#     "a": 9,
+#     "b": 8,
+# }
+#
+# final_dictionary = {
+#     "a": 9,
+#     "b": 8,
+#     "c": 7,
+# }
+#
+# starting_dictionary["c"] = 7
+# final_dictionary = starting_dictionary
+
+# Q1 - Answer - 5
+
+
+# dict = {
+#     "a": 1,
+#     "b": 2,
+#     "c": 3,
+# }
+#
+# print(dict[1])
+
+# Q2 - Answer - 4
+
+# order = {
+#     "starter": {1: "Salad", 2: "Soup"},
+#     "main": {1: ["Burger", "Fries"], 2: ["Steak"]},
+#     "dessert": {1: ["Ice Cream"], 2: []},
+# }
+#
+# print(order["main"][2][0])
+
+# Q3 - Answer - 4

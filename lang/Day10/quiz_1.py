0c0133e45af3b587b14062f2e8ecc1aad0b02ffc
Paul McQuade <paulmcquad@gmail.com>
2025-09-17 22:15:22 +0100

    Day 10 - Quiz
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day10/quiz_1.py
@@ -0,0 +1,40 @@
+#Q1 -> 12.0
+
+# def add(n1, n2):
+#     return n1 + n2
+#
+# def subtract(n1, n2):
+#     return n1 - n2
+#
+# def multiply(n1, n2):
+#     return n1 * n2
+#
+# def divide(n1, n2):
+#     return n1 / n2
+#
+# print(add(2, multiply(5, divide(8, 4))))
+
+#Q2 - 15
+#
+# def outer_function(a, b):
+#     def inner_function(c, d):
+#         return c + d
+#
+#     return inner_function(a, b)
+#
+#
+# result = outer_function(5, 10)
+# print(result)
+
+# Q3 - None
+
+# def my_function(a):
+#     if a < 40:
+#         return
+#         print("Terrible")
+#     if a < 80:
+#         return "Pass"
+#     else:
+#         return "Great"
+#
+# print(my_function(25))

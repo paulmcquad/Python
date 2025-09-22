a73155afb9cdfcbe5c22e4b4073b60eb878d7285
Paul McQuade <paulmcquad@gmail.com>
2025-09-22 19:01:38 +0100

    Day 12 - Scope Quiz
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day12/scope_quiz.py
@@ -0,0 +1,32 @@
+#Q1 - NameError
+
+# def a_function(a_parameter):
+#     a_variable = 15
+#     return a_parameter
+#
+#
+# a_function(10)
+# print(a_variable)
+
+#Q2 - 50
+
+# i = 50
+#
+# def foo():
+#     i = 100
+#     return i
+#
+# foo()
+# print(i)
+
+#Q3 - 16
+
+# def bar():
+#     my_variable = 9
+#
+#     if 16 > 9:
+#         my_variable = 16
+#
+#     print(my_variable)
+#
+# bar()

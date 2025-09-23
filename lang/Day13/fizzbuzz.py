576e5cdec499fdc80c26d4d64af58a2c1d59ebfd
Paul McQuade <paulmcquad@gmail.com>
2025-09-23 22:08:31 +0100

    Day 13
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day13/fizzbuzz.py
@@ -0,0 +1,13 @@
+# Target is the number up to which we count
+def fizz_buzz(target):
+    for number in range(1, target + 1):
+        if number % 3 == 0 and number % 5 == 0:
+            print("FizzBuzz")
+        elif number % 3 == 0:
+            print("Fizz")
+        elif number % 5 == 0:
+            print("Buzz")
+        else:
+            print(number)
+
+print(fizz_buzz(5))
---
+++ lang/Day13/odd_or_even.py
@@ -0,0 +1,8 @@
+def odd_or_even(number):
+    if number % 2 == 0:
+        return "This is an even number."
+    else:
+        return "This is an odd number."
+
+print(odd_or_even(12))
+print(odd_or_even(13))

90e87e6efeae1fe7c01ceb350332ed67e32a4d0f
Paul McQuade <paulmcquad@gmail.com>
2025-09-25 23:24:22 +0100

    Day 16 - OOP Coffee
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day16/main_objects.py
@@ -0,0 +1,19 @@
+#Q1 - In OOP what is the name of the blueprint for creating objects?
+#A1 - Class
+
+#Q2 - Given a Class blueprint for a Car has the following attributes and methods,
+#     which line of code in the answers will produce an error?
+#Attributes:
+#num_of_seats
+#speed
+#Methods:
+#drive()
+#brake()
+
+#A2 - car.brake() = 0
+
+#Q3 - my_toyota = Car()
+#     my_fiat = Car()
+# What word would you use to describe what's inside my_toyota and my_fiat?
+
+#A3 - Object
---
+++ lang/Day16/main_prettytable.py
@@ -0,0 +1,9 @@
+from prettytable import PrettyTable
+table = PrettyTable()
+table.add_column("Pokemon Name",
+["Pikachu", "Squirtle", "Charmander"])
+table.add_column("Type",
+["Electric","Water","Fire"])
+
+table.align = "l"
+print(table)

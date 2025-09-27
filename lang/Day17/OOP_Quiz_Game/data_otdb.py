d21b4b2815faf1a24eaae3d5602e9c27cb70c99c
Paul McQuade <paulmcquad@gmail.com>
2025-09-27 20:19:01 +0100

    Day 17 - OOP_Quiz_Game
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day17/OOP_Quiz_Game/data_otdb.py
@@ -0,0 +1,102 @@
+question_data = [
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "JavaScript derives from a later version of Java",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "A Mac is not a PC",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "The first IBM PC was released in 1981.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "&quot;HTML&quot; stands for Hypertext Markup Language.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "Linus Torvalds created Linux and Git.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "Ada Lovelace is often considered the first computer programmer.",
+      "correct_answer": "True",
+      "incorrect_answers": [
+        "False"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "The logo for Snapchat is a Bell.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "Pointers were not used in the original C programming language; they were added later on in C++.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "Time on Computers is measured via the EPOX System.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    },
+    {
+      "type": "boolean",
+      "difficulty": "easy",
+      "category": "Science: Computers",
+      "question": "Linux was first created as an alternative to Windows XP.",
+      "correct_answer": "False",
+      "incorrect_answers": [
+        "True"
+      ]
+    }
+]
--- lang/Day17/OOP_Quiz_Game/main_otdb.py
+++ lang/Day17/OOP_Quiz_Game/main_otdb.py
@@ -1,10 +1,15 @@
 from question_model import Question
-from data import question_data
+from data_otdb import question_data
 from quiz_brain import QuizBrain
+
+# Open Trivia Database
+# https://opentdb.com/api_config.php
+# JSON to Python Dict
+# https://json-to.com/json-python
 
 question_bank = []
 for question in question_data:
-    new_question = Question(question["text"], question["answer"])
+    new_question = Question(question["question"], question["correct_answer"])
     question_bank.append(new_question)
 
 quiz = QuizBrain(question_bank)

6f69134bfb913d844549523a430f5576de6da79d
Paul McQuade <paulmcquad@gmail.com>
2025-10-23 22:28:05 +0100

    Day 24 - Files / Mail Merge
    Signed-off-by: Paul McQuade <paulmcquad@gmail.com>

---
+++ lang/Day24/Files/main_file.py
@@ -0,0 +1,15 @@
+# Reading Files
+
+with open("my_file.txt") as file:
+    contents = file.read()
+    print(contents)
+
+# Writing to Files
+
+with open("my_file.txt", mode="w") as file:
+    file.write("New text.")
+
+# Opening a File that doesn't exit in write mode will create it from scratch
+
+with open("file_that_doesnt_exist.txt", mode="w") as file:
+    file.write("New text.")
